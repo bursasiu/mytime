868daafbbceb19c67a86e6a5b88f418ee6f40501
Cristian Bursasiu <bursasiu@gmail.com>
2016-05-22 00:09:51 +0300

    test01

---
+++ mytime/src/mytime/user.java
@@ -0,0 +1,21 @@
+package mytime;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class user {
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	String name;
+	String email;
+}
